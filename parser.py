e6c861bc36f03caa46b50bc5e2f17fb5a00b28de
Mafer Mazu <13-10787@usb.ve>
2020-02-09 14:28:08 -0400

    parser update

--- parser.py
+++ parser.py
@@ -3,3 +3,33 @@ DEBUG_MODE = False
 
 # import some required globals from tokenizer
 tokens = lexer.tokens
+
+def p_intanceWorld(p):
+    '''instanceWorld : TkBeginWorld Tkid wordlSet instructions TkEndWorld instanceWorld |
+                       TkBeginWorld Tkid wordlSet instructions startRobotAt instructions TkEndWorld instanceWorld | 
+                       empty'''
+    pass
+
+def p_worldSet(p):
+    'worldSet : TkWorld TkInt TkInt | empty'
+    pass
+
+def p_instructions(p):
+    '''instructions : wallSet instructions |
+                      objectTypeSet instructions | 
+                      placeInBasketSet instructions |
+                      placeInWorldSet instructions |
+                      
+    '''
+    pass
+
+def p_wallSet(p):
+    'wallSet : TkWall directions TkFrom TkInt TkInt TkTo TkInt TkInt'
+    pass
+
+def p_directions(p):
+    'directions : TkNorth | TkEast | TkSouth | TkWest'
+    pass
+def p_empty(p):
+    'empty :'
+    pass
