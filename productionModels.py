bb9adfe709fd8fbf931140aa6a4839feb463cbef
Mafer Mazu <13-10787@usb.ve>
2020-02-16 16:01:52 -0400

    all class in productionModels

--- productionModels.py
+++ productionModels.py
@@ -41,7 +41,7 @@ class objectType:
         self.tkColor = tkColor
         self.color = color
 
-class sePlace:
+class setPlace:
     def __init__(self, tkPlace, amount, tkOf, tkIdObject, tkAt, col, row):
         self.tkPlace = tkPlace
         self.amount = amount
@@ -106,3 +106,97 @@ class goalObjectAtPlace:
         self.tkAt = tkAt
         self.col = col
         self.row = row
+
+
+class instanceTask:
+    def __init__(self, tkBeginTask, id, tkOn, worldId, taskInstructions, tkEndTask):
+        self.tkBeginTask = tkBeginTask
+        self.id = id
+        self.tkOn = tkOn
+        self.worldId = worldId
+        self.taskInstructions = taskInstructions
+        self.tkEndTask = tkEndTask
+
+class ifThen:
+    def __init__(self, tkIf, tkBool, tkThen, instruction):
+        self.tkIf = tkIf
+        self.tkBool = tkBool
+        self.tkThen = tkThen
+        self.instructions = instruction
+
+class ifThenElse:
+    def __init__(self, tkIf, tkBool, tkThen, instruction,tkElse, instruction2):
+        self.tkIf = tkIf
+        self.tkBool = tkBool
+        self.tkThen = tkThen
+        self.instructions = instruction
+        self.tkThen = tkThen
+        self.instructions2 = instruction2
+
+class repeat:
+    def __init__(self, tkInt, tkTimes, instruction):
+        self.tkInt = tkInt
+        self.tkTimes = tkTimes
+        self.instruction = instruction
+
+class whileLoop:
+    def __init__(self, tkWhile, tkBool, tkDo, instruction):
+        self.tkWhile = tkWhile
+        self.tkBool = tkBool
+        self.tkDo = tkDo
+        self.instruction = instruction
+
+class beginIntructions:
+    instructions = []
+    def __init__(self, tkBegin, instruction, tkEnd):
+        self.tkBegin = tkBegin
+        self.instructions = instruction
+        self.tkEnd = tkEnd
+
+class defineInstructions:
+    def __init__(self, tkDefine, tkId, tkAs, instruction):
+        self.tkDefine = tkDefine
+        self.tkId = tkId
+        self.tkAs = tkAs
+        self.instruction = instruction
+
+class firstIf:
+    def __init__(self, tkIf, tkBool, tkThen, instruction):
+        self.tkIf = tkIf
+        self.tkBool = tkBool
+        self.tkThen = tkThen
+        self.instructions = instruction
+
+class singleIdInst:
+    def __init__(self, tkInst, id):
+        self.tkInst = tkInst
+        self.id = id
+
+class dualIdInst:
+    def __init__(self, instruction, id1, id2):
+        self.instrcution = instruction
+        self.id1 = id1
+        self.id2 = id2
+
+class changeBool:
+    def __init__(self, tkSet, id, valueIn, valueOut, id2):
+        self.tkSet = tkSet
+        self.id = id
+        self.valueIn = valueIn
+        self.valueOut = valueOut
+        self.id2 = id2
+
+class testBools:
+    def __init__(self, bool1, oper, bool2):
+        self.bool1 = bool1
+        self.oper = oper
+        self.bool2 = bool2
+
+class sigleOper:
+    def __init__(self, oper, bool):
+        self.oper = oper
+        self.bool = bool
+
+class  sigleton:
+    def __init__(self, bool):
+        self.bool = bool
