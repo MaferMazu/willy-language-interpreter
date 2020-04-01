bfb44ebf28ea069bdf08e1d6e5e6330bb0db5b26
Mafer Mazu <13-10787@usb.ve>
2020-04-01 01:32:48 -0400

    impresion sexy ast

--- Node.py
+++ Node.py
@@ -8,10 +8,11 @@ class Node:
           self.leaf = leaf
 
      def __str__(self, level=0):
-          ret = " " * level + repr(self.type) + "\n"
+          ret = "  " * level + self.type + ":" + "\n"
           for child in self.children:
                if isinstance(child,Node):
                     ret += child.__str__(level + 1)
                else:
-                    ret += "\t" * level + repr(self.type) + "\n"
+                    ret = ret.rstrip("\n")
+                    ret += " " + str(child) + "\n"
           return ret
