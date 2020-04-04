e4ff354846cd4f9393f2a32d78e869b5555432c4
Mafer Mazu <13-10787@usb.ve>
2020-04-03 23:00:52 -0400

    Task

--- Task.py
+++ Task.py
@@ -2,3 +2,9 @@ class Task:
     def __init__(self,World):
         self.instructions="" #todas mis instrucciones
         self.world=World
+
+    def getInstructions(self):
+        return self.instructions
+
+    def setInstructions(self,input):
+        self.instructions=self.instructions + input
