91ab23d97720a3e1260129b994911a123f8ce15a
Mafer Mazu <13-10787@usb.ve>
2020-04-04 23:46:10 -0400

    define as, falta booleanTest editar bollValue en Node.py

--- Node.py
+++ Node.py
@@ -1,4 +1,5 @@
 from World import *
+from Task import *
 class Node:
      def __init__(self,type,children=None,leaf=None):
           self.type = type
@@ -67,3 +68,92 @@ class Node:
                return mybool
           else:
                return False
+
+     def boolValue(self,mundo):
+          mybool = True
+          if isinstance(mundo,World):
+               print(self.type)
+               if self.type=="Conjuncion":
+                    left = self.children[0].boolValue(mundo)
+                    rigth = self.children[1].boolValue(mundo)
+                    mybool= mybool and (mundo.getValueGoals(left) and mundo.getValueGoals(rigth))
+               elif self.type=="Disyuncion":
+                    left = self.children[0].boolValue(mundo)
+                    rigth = self.children[1].boolValue(mundo)
+                    mybool= mybool and (mundo.getValueGoals(left) or mundo.getValueGoals(rigth))
+               elif self.type=="Parentesis":
+                    u = self.children[0].boolValue(mundo)
+                    mybool= mybool and ((u))
+               elif self.type=="Not":
+                    u = self.children[0].boolValue(mundo)
+                    mybool= mybool and (not u)
+               elif self.type=="Found":
+                    mybool= mybool and mundo.isCellWithObject(mundo.getWillyPosition()[0],self.children[0])
+               else:
+                    for child in self.children:
+                         if isinstance(child,Node):
+                              mybool= mybool and (child.boolValue(mundo))
+                         else:
+                              print("soy leaf y mi valor es:",child,mundo.getGoals(),mundo.getValueGoals(child))
+                              mybool=mybool and mundo.getValueBool(child)
+                    
+               return mybool
+          else:
+               return False
+
+
+     def executeMyTask(self,task):
+          
+          if isinstance(task,Task):
+               print(self.type)
+
+               if self.type=="Drop":
+                    if task.world.isObjectBasket(self.children[0]) and task.world.isObject(self.children[0]):
+                         task.dropObject(self.children[0])
+               elif self.type=="Pick":
+                    if task.world.isCellWithObject(task.world.getWillyPosition()[0],self.children[0]) and task.world.isObject(self.children[0]):
+                         task.pickObject(self.children[0])
+               elif self.type=="Clear":
+                    task.world.changeBool(self.children[0], False)
+               elif self.type=="Flip":
+                    boolAux = task.world.getValueBool(self.children[0])
+                    task.world.changeBool(self.children[0], not boolAux)
+               elif self.type=="SetBool":
+                    task.world.changeBool(self.children[0],self.children[1])
+               elif self.type=="SetTrue":
+                    task.world.changeBool(self.children[0],True)
+               elif self.type=="Move":
+                    task.moveWilly()
+               elif self.type=="TL":
+                    task.turnWilly("left")
+               elif self.type=="TR":
+                    task.turnWilly("right")
+               elif self.type=="Terminate":
+                    print(task.world)
+               elif self.type=="ifSimple":
+                    if self.children[0].boolValue(task.world):
+                         self.children[1].executeMyTask(task)
+               elif self.type=="ifCompound":
+                    if self.children[0].boolValue(task.world):
+                         self.children[1].executeMyTask(task)
+                    else:
+                         self.children[2].executeMyTask(task)
+               elif self.type =="whileInst":
+                    while self.children[0].boolValue(task.world):
+                         self.children[1].executeMyTask(task)
+               elif self.type =="Define As":
+                    task.instructions.append([self.children[0].children[0],self.children[1]])
+
+               else:
+                    defineas=False
+                    for x in task.instructions:
+                         if self.type==x[0]:
+                              defineas = True
+                              x[1].executeMyTask(task)
+                    if not defineas:
+                         for child in self.children:
+                              if isinstance(child,Node):
+                                   child.executeMyTask(task)
+                              else:
+                                   print("soy leaf y mi valor es:",child)
+                                   mybool=mybool and task.world.getValueGoals(child)
