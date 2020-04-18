28a273e98a56595a05f828ad315270d463de24b4
Jesus Marcano <jmarcano@ubiipagos.com>
2020-04-18 00:33:07 -0400

    makefileready

--- makefile.sh
+++ makefile.sh
@@ -1,28 +1,24 @@
 chmod a+x main.py
-DIR=~/local1/
-PWD=$(pwd)
-echo $PWD
+DIR=~/.local/bin
 if [ -d "$DIR" ]; then
     echo "$DIR exist"
 
 else
     echo "$DIR does not exist"
-    mkdir ~/local1/
+    mkdir ~/.local/bin
     echo "$DIR NOW CREATED"
 fi
-cp main.py ~/local1/
-ln -s main.py $PWD/willy
+cp *.py ~/.local/bin
+ln -s main.py ~/.local/bin/willy
 FILE=~/.bash_profile
-
 if [ -f "$FILE" ]; then
     echo "$FILE exist"
-    echo -e 'export PATH="$PATH:'$PWD'/willy"' >> ~/.bash_profile
+    echo -e 'export PATH="$PATH:~/.local/bin"' » ~/.bash_profile
 else
     echo "$FILE does not exist"
     touch ~/.bash_profile
-    echo -e 'export PATH="$PATH:'$PWD'/willy"' >> ~/.bash_profile
+    echo -e 'export PATH="$PATH:~/.local/bin"' » ~/.bash_profile
     echo "$FILE NOW CREATED"
 fi
-
 source ~/.bash_profile
-echo $PATH
+echo "command willy has been added to ysudour path"
